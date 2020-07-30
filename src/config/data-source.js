15c73bda6eb4533d7ef01dd3db9dbf4a650da8d2
Luciano Lima <luclimasilva@hotmail.com>
2020-07-30 12:19:47 -0300

    add configs

--- src/config/data-source.js
+++ src/config/data-source.js
@@ -3,6 +3,14 @@ db.Promise = global.Promise
 
 console.log('Successful database connection...')
 
-module.exports = db.connect('mongodb://root:mongo1234@localhost:27017/mymoneydb', {
-    auth: { authdb:"admin" }
-})
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://root:mongo1234@localhost:27017/mymoneydb'
+
+module.exports = db.connect(url, { auth: { authdb:"admin" }, useMongoClient: true })
+
+db.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+db.Error.messages.Number.min = 
+    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+db.Error.messages.Number.max = 
+    "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+db.Error.messages.String.enum = 
+    "'{VALUE}' não é válido para o atributo '{PATH}'."
