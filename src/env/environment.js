6268a505d096eef30e81a665d0964a4dde78acd0
Luciano Lima <luclimasilva@hotmail.com>
2020-08-05 22:00:41 -0300

    v3

--- src/env/environment.js
+++ src/env/environment.js
@@ -2,7 +2,7 @@ module.exports = {
     authSecret: process.env.AUTH_SECRET,
     url: process.env.MONGODB_LAB_URI,
     port: process.env.PORT
-    
+
     // authSecret: '2dca2d5f-ee9b-4302-955b-45f78e6b6c6c',
     // url: 'mongodb://root:mongo1234@localhost:27017/mymoneydb?authSource=admin',
     // port: 5000
